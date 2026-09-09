zydo/alle

Rebase the macOS app onto main as an additive change
dev/macos-app now sits directly on main's HEAD with the macOS client
re-added as one change: the Swift menu-bar app (macos/Alle/), the
companion/tray clients (alle.companion, alle.tray), the hermetic .pkg
packaging (packaging/macos/), their tests and installer doc, plus the
macos build group and headless-wheel guards the code needs.

The retargeted product promises on main (status, getting-started,
cli-reference) are kept as-is; only the macOS subsystem is layered on.

src/alle/api/server.py
@@ -453,10 +453,11 @@ def _api_secret(api: dict) -> str:
 
 def effective_api() -> dict:
     """The client-facing endpoint after env overrides: ``{"address", "secret",
-    "host", "net"}`` — what same-machine tools (``alle ui``) use.
+    "host", "net"}`` — what same-machine tools (``alle ui``, the tray) use.
 
-    May generate the loopback contract file; read-only callers compose
-    :func:`_listen_config`/:func:`_api_secret` over their own read instead.
+    May generate the loopback contract file; read-only callers (the
+    companion) compose :func:`_listen_config`/:func:`_api_secret` over their
+    own read instead.
     """
     api = control_api()
     lc = _listen_config(api)

anyhop/anyhop

src/anyhop/api/server.py
@@ -453,10 +453,11 @@ def _api_secret(api: dict) -> str:
 
 def effective_api() -> dict:
     """The client-facing endpoint after env overrides: ``{"address", "secret",
-    "host", "net"}`` — what same-machine tools (``anyhop ui``) use.
+    "host", "net"}`` — what same-machine tools (``anyhop ui``, the tray) use.
 
-    May generate the loopback contract file; read-only callers compose
-    :func:`_listen_config`/:func:`_api_secret` over their own read instead.
+    May generate the loopback contract file; read-only callers (the
+    companion) compose :func:`_listen_config`/:func:`_api_secret` over their
+    own read instead.
     """
     api = control_api()
     lc = _listen_config(api)